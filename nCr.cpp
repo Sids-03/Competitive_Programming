70d960fda352d136f0ff993c330aaf97dcde1346
Sids-03 <110683058+Sids-03@users.noreply.github.com>
2022-09-30 18:46:10 +0530

    Create nCr.cpp

---
+++ nCr.cpp
@@ -0,0 +1,34 @@
+
+struct nCr{
+    ll maxx , md; 
+    vll fact, ifact; 
+    inline ll mul(ll a, ll b) { return a *1LL* b % md ;}
+    ll power(ll a, ll n) {
+        if(n == 0) return 1 ; 
+        int p = power(a, n/2) % md ; 
+        p = mul(p, p) ;  
+        return n & 1 ? mul(p, a) : p ; 
+    }
+    int invMod(int a) {return power(a,md-2);}
+    void pre() {
+        fact[0] = 1 ;
+        for(int i = 1;i< maxx;++i) fact[i] = mul(i, fact[i-1]) ;
+        ifact[maxx-1] = invMod(fact[maxx-1]) ; 
+        for(int i = maxx-1 ; i>0 ;--i) ifact[i-1] = mul(ifact[i], i) ;
+    }
+    nCr(int _mxN, int _M) {
+        maxx = _mxN + 1; 
+        md = _M ; 
+        fact.resize(maxx) ; 
+        ifact.resize(maxx) ;
+        pre() ;  
+    }
+    ll C(ll n, ll r) {
+        if (n < r || r < 0 || n < 0) return 0;
+        return mul(fact[n], mul(ifact[r], ifact[n-r])) ;
+    }
+};
+//maxx N we need
+//const int N = 100;
+// initialise nCr struct
+// nCr comb(N , mod);
