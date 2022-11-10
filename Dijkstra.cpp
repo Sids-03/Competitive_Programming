d6b95bb35713938b1caf2e97608162af9d4f09ea
Sids-03 <110683058+Sids-03@users.noreply.github.com>
2022-11-10 14:58:59 +0530

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,28 +1,24 @@
- 
   ll n;
   vector<vector<pll>> adj;
   vll d , parent;
   
-  
   void dijkstra(ll source){
-       adj.resize(n+1);
-       parent.resize(n+1);
-       d.resize(n+1);
+  	adj.resize(n+1);
+  	parent.resize(n+1);
+  	d.resize(n+1);
       priority_queue<pll> pq;
-     pq.push({0,source});
+      pq.push({0,source});
        while(!pq.empty()){
            ll vert = pq.top().second;
            ll dist = pq.top().first;
            pq.pop();
            if(dist > d[vert])continue;
            for(auto it:adj[vert]){
-               if(d[vert]+dist < d[it.first]){
-                  d[it.first] = d[vert] + dist;
+               if(d[vert]+it.second < d[it.first]){
+                  d[it.first] = d[vert] + it.second;
                    pq.push({d[it.first] , it.first});
-                 parent[it.first] = vert;
+                   parent[it.first] = vert;
               }
           }
       }
- 
- 
   }
