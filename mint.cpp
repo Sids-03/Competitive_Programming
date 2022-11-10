49c17eee4c72059d360f8fded6068585ce5be783
Sids-03 <110683058+Sids-03@users.noreply.github.com>
2022-11-10 14:53:13 +0530

    Create mint.cpp

---
+++ mint.cpp
@@ -0,0 +1,78 @@
+ll norm(ll x) {
+    if (x < 0) {
+        x += mod;
+    }
+    if (x >= mod) {
+        x -= mod;
+    }
+    return x;
+}
+template<class T>
+T modpower(T a, ll b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+struct mint {
+    ll x;
+    mint(int x = 0) : x(norm(x)) {}
+    mint(ll x) : x(norm(x % mod)) {}
+    ll val() const {
+        return x;
+    }
+    mint operator-() const {
+        return mint(norm(mod - x));
+    }
+    mint inv() const {
+        assert(x != 0);
+        return modpower(*this, mod - 2);
+    }
+    mint &operator*=(const mint &rhs) {
+        x = ll(x) * rhs.x % mod;
+        return *this;
+    }
+    mint &operator+=(const mint &rhs) {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    mint &operator-=(const mint &rhs) {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    mint &operator/=(const mint &rhs) {
+        return *this *= rhs.inv();
+    }
+    friend mint operator*(const mint &lhs, const mint &rhs) {
+        mint res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend mint operator+(const mint &lhs, const mint &rhs) {
+        mint res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend mint operator-(const mint &lhs, const mint &rhs) {
+        mint res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend mint operator/(const mint &lhs, const mint &rhs) {
+        mint res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend std::istream &operator>>(std::istream &is, mint &a) {
+        ll v;
+        is >> v;
+        a = mint(v);
+        return is;
+    }
+    friend std::ostream &operator<<(std::ostream &os, const mint &a) {
+        return os << a.val();
+    }
+};
