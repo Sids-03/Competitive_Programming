8974a05c46fc3da6cfecd4ef3ee3ba22878e7a9c
Sids-03 <110683058+Sids-03@users.noreply.github.com>
2022-12-31 01:07:12 +0530

    Update mint.cpp

--- mint.cpp
+++ mint.cpp
@@ -1,78 +1,194 @@
-ll norm(ll x) {
-    if (x < 0) {
-        x += mod;
-    }
-    if (x >= mod) {
-        x -= mod;
-    }
-    return x;
+//use Mint and value at ans.x
+//Credit: Um_nik , Link: https://codeforces.com/contest/1770/submission/187276330
+
+ll myRand(ll B) {
+	return (ull)rng() % B;
 }
-template<class T>
-T modpower(T a, ll b) {
-    T res = 1;
-    for (; b; b /= 2, a *= a) {
-        if (b % 2) {
-            res *= a;
-        }
-    }
-    return res;
-}
-struct mint {
-    ll x;
-    mint(int x = 0) : x(norm(x)) {}
-    mint(ll x) : x(norm(x % mod)) {}
-    ll val() const {
-        return x;
-    }
-    mint operator-() const {
-        return mint(norm(mod - x));
-    }
-    mint inv() const {
-        assert(x != 0);
-        return modpower(*this, mod - 2);
-    }
-    mint &operator*=(const mint &rhs) {
-        x = ll(x) * rhs.x % mod;
-        return *this;
-    }
-    mint &operator+=(const mint &rhs) {
-        x = norm(x + rhs.x);
-        return *this;
-    }
-    mint &operator-=(const mint &rhs) {
-        x = norm(x - rhs.x);
-        return *this;
-    }
-    mint &operator/=(const mint &rhs) {
-        return *this *= rhs.inv();
-    }
-    friend mint operator*(const mint &lhs, const mint &rhs) {
-        mint res = lhs;
-        res *= rhs;
-        return res;
-    }
-    friend mint operator+(const mint &lhs, const mint &rhs) {
-        mint res = lhs;
-        res += rhs;
-        return res;
-    }
-    friend mint operator-(const mint &lhs, const mint &rhs) {
-        mint res = lhs;
-        res -= rhs;
-        return res;
-    }
-    friend mint operator/(const mint &lhs, const mint &rhs) {
-        mint res = lhs;
-        res /= rhs;
-        return res;
-    }
-    friend std::istream &operator>>(std::istream &is, mint &a) {
-        ll v;
-        is >> v;
-        a = mint(v);
-        return is;
-    }
-    friend std::ostream &operator<<(std::ostream &os, const mint &a) {
-        return os << a.val();
-    }
+
+ 
+const ll MOD = 998244353;
+template<ll mod = MOD> struct mint { // 1000000007  1000000009
+	ll x;
+ 
+	mint() : x(0) {}
+	mint(ll _x) {
+		_x %= mod;
+		if (_x < 0) _x += mod;
+		x = _x;
+	}
+ 
+	mint& operator += (const mint &a) {
+		x += a.x;
+		if (x >= mod) x -= mod;
+		return *this;
+	}
+	mint& operator -= (const mint &a) {
+		x += mod - a.x;
+		if (x >= mod) x -= mod;
+		return *this;
+	}
+	mint& operator *= (const mint &a) {
+		x = (ull)x * a.x % mod;
+		return *this;
+	}
+	mint pow(ll pw) const {
+		mint res = 1;
+		mint cur = *this;
+		while(pw) {
+			if (pw & 1) res *= cur;
+			cur *= cur;
+			pw >>= 1;
+		}
+		return res;
+	}
+	mint inv() const {
+		assert(x != 0);
+		ll t = x;
+		ll res = 1;
+		while(t != 1) {
+			ll z = mod / t;
+			res = (ull)res * (mod - z) % mod;
+			t = mod - t * z;
+		}
+		return res;
+	}
+	mint& operator /= (const mint &a) {
+		return *this *= a.inv();
+	}
+	mint operator + (const mint &a) const {
+		return mint(*this) += a;
+	}
+	mint operator - (const mint &a) const {
+		return mint(*this) -= a;
+	}
+	mint operator * (const mint &a) const {
+		return mint(*this) *= a;
+	}
+	mint operator / (const mint &a) const {
+		return mint(*this) /= a;
+	}
+ 
+	bool sqrt(mint &res) const {
+		if (mod == 2 || x == 0) {
+			res = *this;
+			return true;
+		}
+		if (pow((mod - 1) / 2) != 1) return false;
+		if (mod % 4 == 3) {
+			res = pow((mod + 1) / 4);
+			return true;
+		}
+		int pw = (mod - 1) / 2;
+		int K = 30;
+		while((1 << K) > pw) K--;
+		while(true) {
+			mint t = myRand(mod);
+			mint a = 0, b = 0, c = 1;
+			for (int k = K; k >= 0; k--) {
+				a = b * b;
+				b = b * c * 2;
+				c = c * c + a * *this;
+				if (((pw >> k) & 1) == 0) continue;
+				a = b;
+				b = b * t + c;
+				c = c * t + a * *this;
+			}
+			if (b == 0) continue;
+			c -= 1;
+			c *= mint() - b.inv();
+			if (c * c == *this) {
+				res = c;
+				return true;
+			}
+		}
+		assert(false);
+	}
+ 
+	bool operator == (const mint &a) const {
+		return x == a.x;
+	}
+	bool operator != (const mint &a) const {
+		return x != a.x;
+	}
+	bool operator < (const mint &a) const {
+		return x < a.x;
+	}
 };
+template<ll mod = MOD> struct Factorials {
+	using Mint = mint<mod>;
+	vector<Mint> f, fi;
+ 
+	Factorials() : f(), fi() {}
+	Factorials(int n) {
+		n += 10;
+		f = vector<Mint>(n);
+		fi = vector<Mint>(n);
+		f[0] = 1;
+		for (int i = 1; i < n; i++)
+			f[i] = f[i - 1] * i;
+		fi[n - 1] = f[n - 1].inv();
+		for (int i = n - 1; i > 0; i--)
+			fi[i - 1] = fi[i] * i;
+	}
+ 
+	Mint C(int n, int k) {
+		if (k < 0 || k > n) return 0;
+		return f[n] * fi[k] * fi[n - k];
+	}
+};
+template<ll mod = MOD> struct Powers {
+	using Mint = mint<mod>;
+	vector<Mint> p, pi;
+ 
+	Powers() : p(), pi() {}
+	Powers(int n, Mint x) {
+		n += 10;
+		if (x == 0) {
+			p = vector<Mint>(n);
+			p[0] = 1;
+		} else {
+			p = vector<Mint>(n);
+			pi = vector<Mint>(n);
+			p[0] = pi[0] = 1;
+			Mint xi = x.inv();
+			for (int i = 1; i < n; i++) {
+				p[i] = p[i - 1] * x;
+				pi[i] = pi[i - 1] * xi;
+			}
+		}
+	}
+ 
+	Mint pow(int n) {
+		if (n >= 0)
+			return p[n];
+		else
+			return pi[-n];
+	}
+};
+template<ll mod = MOD> struct Inverses {
+	using Mint = mint<mod>;
+	vector<Mint> ii;
+ 
+	Inverses() : ii() {}
+	Inverses(int n) {
+		n += 10;
+		ii = vector<Mint>(n);
+		ii[1] = 1;
+		for (int x = 2; x < n; x++)
+			ii[x] = Mint() - ii[mod % x] * (mod / x);
+	}
+ 
+	Mint inv(Mint x) {
+		assert(x != 0);
+		ll t = x.x;
+		ll res = 1;
+		while(t >= (int)ii.size()) {
+			ll z = mod / t;
+			res = (ull)res * (mod - z) % mod;
+			t = mod - t * z;
+		}
+		return ii[t] * res;
+	}
+};
+using Mint = mint<>;
+ 
