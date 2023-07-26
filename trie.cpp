5f23929c55a8f23965c501a2f615ac201c022e3e
Sids-03 <110683058+Sids-03@users.noreply.github.com>
2023-07-26 15:26:34 +0530

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,56 +1,65 @@
-template< int MXBIT> struct Trie{
-    struct Node{
-        int end_cnt = 0;
-        int idx;
-        int next[MXBIT] = {-1 , -1};
-    };
-    vector<Node>nodes;
-    Node def;
-    int k;
-    Trie(int K){
-        k = K;
-        nodes.push_back(def);
-    }
-
-    void prints(){
-        cout << nodes.size() << endl;
-        for(int i = 0 ; i < nodes.size() ; i ++){
-            cout << nodes[i].next[0] << " " << nodes[i].next[1] << endl;
-        }
-    }
-
-    void add(string s , int id){
-        int curr = 0;
-        for(auto ch: s){
-            if(nodes[curr].next[ch-'0'] == -1){
-                nodes[curr].next[ch-'0'] = nodes.size();
-                nodes.push_back(def);
+template<int MXBIT> 
+    struct Trie{
+        struct Node{
+            int end_cnt = 0;
+            int idx;
+            int next[MXBIT];
+            Node(){
+                for(int i = 0 ; i < MXBIT ; i ++) next[i] = -1;
             }
-            curr = nodes[curr].next[ch-'0'];
+        };
+        vector<Node>nodes;
+        Node def;
+        int k;
+        Trie(int K = 0){
+            k = K;
+            nodes.push_back(def);
         }
-        nodes[curr].idx = id;
-        nodes[curr].end_cnt++;
-    }
-    vector<int> dfs (string s){
-        int x = 0 , curr = 0;
-        int len = k-1;
-        int ans = 0;
-        for(auto ch: s){
-            if(nodes[curr].next[ch-'0'] == -1){
-                curr = nodes[curr].next[(ch-'0')^1];
-            }else{
-                if(ch == '0'){
-                    x ^= (1<<len);
+
+        void add(string s , int id){
+            int curr = 0;
+            for(auto ch: s){
+                if(nodes[curr].next[ch-'a'] == -1){
+                    nodes[curr].next[ch-'a'] = nodes.size();
+                    nodes.push_back(def);
                 }
-                curr = nodes[curr].next[ch-'0'];
-                ans ^= (1<<len);
+                curr = nodes[curr].next[ch-'a'];
             }
-            --len;
+            nodes[curr].idx = id;
+            nodes[curr].end_cnt++;
         }
-        int id = nodes[curr].idx;
-        return {ans, x, id};
+        bool search(string word){
+            int curr = 0;
+            for(auto ch: word){
+                if(nodes[curr].next[ch-'a'] == -1){
+                    return false;
+                }else{
+                    curr = nodes[curr].next[ch-'a'];
+                }
+            }
+            return (bool)nodes[curr].end_cnt;
+        }
+        vector<int> dfs (string s){
+            int x = 0 , curr = 0;
+            int len = k-1;
+            int ans = 0;
+            for(auto ch: s){
+                if(nodes[curr].next[ch-'a'] == -1){
+                    curr = nodes[curr].next[(ch-'a')^1];
+                }else{
+                    if(ch == '0'){
+                        x ^= (1<<len);
+                    }
+                    curr = nodes[curr].next[ch-'a'];
+                    ans ^= (1<<len);
+                }
+                --len;
+            }
+            int id = nodes[curr].idx;
+            return {ans, x, id};
 
-    }
-};
+        }
+    };
 
-// submission: https://codeforces.com/contest/1851/submission/215701055
+//submission: https://codeforces.com/contest/1851/submission/215701055
+//submission2: https://leetcode.com/problems/word-break/submissions/1004336379/
