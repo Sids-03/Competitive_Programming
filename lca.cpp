f23ee25b641f4a1f768a7dedc09ea1779e276820
Sids-03 <110683058+Sids-03@users.noreply.github.com>
2023-09-23 10:54:53 +0530

    Update lca.cpp

--- lca.cpp
+++ lca.cpp
@@ -1,3 +1,4 @@
+
 int n, l;
 vector<vector<int>> adj;
 struct LCA{
@@ -5,15 +6,13 @@ struct LCA{
     vector<vector<int>> up;
     vector<int> tin, tout, depth;
     int timer = 0;
-
     inline void init(int n){
         n++;
         g.resize(n);
         tin.resize(n);
         tout.resize(n);
         depth.resize(n);
-        int l = 31 - __builtin_clz(n);
-        up.assign(n, vector<int>(l + 1, 0));
+        up.assign(n, vector<int>(31, -1));
     }
     LCA(){};
     LCA(int n){
@@ -25,35 +24,35 @@ struct LCA{
         g[v].push_back(u);
     }
 
-    inline void dfs(int v, int p){
-        tin[v] = tout[v] = ++timer;
-        depth[v] = (p == v ? 0 : depth[p] + 1);
+    inline void dfs(int v, int p , int dis){
+        tin[v] = ++timer;
+        depth[v] = dis;
 
         up[v][0] = p;
-        for (int i = 1; i < (int)up[v].size(); i++){
+        for (int i = 1; i < 31; i++){
             up[v][i] = up[up[v][i - 1]][i - 1];
         }
 
         for (int u : g[v]){
             if (u != p){
-                dfs(u, v);
-                tout[v] = tout[u];
+                dfs(u, v , dis+1);
             }
         }
+        tout[v] = ++timer;
     }
 
     void dfs(int root = 0){
-        dfs(root, root);
+        dfs(root, root , 0);
     }
 
     inline bool upper(int u, int v){
         return tin[u] <= tin[v] && tout[v] <= tout[u];
     }
 
-    inline int lca(int u, int v){
+    int lca(int u, int v){
         if (upper(u, v)) return u;
         if (upper(v, u)) return v;
-        for (int i = (int)up[u].size() - 1; i >= 0; i--){
+        for (int i = 30 ; i >= 0; i--){
             if (!upper(up[u][i], v)) u = up[u][i];
         }
         return up[u][0];
