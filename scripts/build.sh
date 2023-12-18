fd00c9d9acf2ba8354c101bd314bb37bc7cb173c
Sids-03 <siddhid21_ug@cse.nits.ac.in>
2023-12-18 11:16:16 +0530

    scipts

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+g++ -static -DLOCAL -lm -s -x c++ -Wall -Wextra -O2 -std=c++17 -o $1 $1.cpp 
+
+#format: build.sh A
---
+++ scripts/stress.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+g++ $1.cpp -o $1.o
+g++ $2.cpp -o $2.o
+g++ $3.cpp -o $3.o
+ 
+for ((testNum=0;testNum<$4;testNum++))
+do
+    ./$3.o > input
+    ./$2.o < input > outSlow
+    ./$1.o < input > outWrong
+    echo "Test case ${testNum} passed"
+    if !(cmp -s "outWrong" "outSlow")
+    then
+        echo "Error found!"
+        echo "Input:"
+        cat input
+        echo "Wrong Output:"
+        cat outWrong
+        echo "Slow Output:"
+        cat outSlow
+        exit
+    fi
+done
+echo Passed $4 tests
